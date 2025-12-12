fe1174934dd02fbf8ba346504c0997acb15875f0
Alexander Mikhailov <extroper@yandex.ru>
2025-12-12 15:40:51 +0300

    refactor(clients): switch to synchronous connections
    - Refactored PostgreSQL, Redis, and RabbitMQ clients to be synchronous, removing async/await patterns.
    - Simplified connection handling, ensuring clients only connect when needed and can be closed properly.
    - Adjusted `BackendUser` tasks to work with the synchronous clients.
    - Fixed the `NoneType` and `coroutine` errors related to Redis and PostgreSQL interactions by ensuring proper synchronous execution.
    
    This change improves performance and stability when running Locust tests with synchronous behavior for all clients.

--- core/constants.py
+++ core/constants.py
@@ -3,7 +3,9 @@ SECONDS_TO_MILLISECONDS = 1000
 QUERY_DATE = "2025-12-04"
 
 SQL_SELECT_COUNT = """
-SELECT count(*) FROM signatures WHERE DATE(signed_at) = %s;
+SELECT count(*)
+FROM signatures
+WHERE DATE(signed_at) = %s;
 """
 
 SQL_INSERT_DOCUMENT = """
